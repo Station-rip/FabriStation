815972a3f6251a0210e7d70b7990e1fad1177aba
eyezah <www.isaiah.ga@gmail.com>
2021-12-28 13:20:49 +1300

    1.17

---
+++ src/main/java/com/eyezah/station/MusicResponses.java
@@ -0,0 +1,134 @@
+package com.eyezah.station;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.MessageType;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+public class MusicResponses {
+
+	public static void send(String command, int type, JsonElement code, ServerCommandSource source) throws CommandSyntaxException {
+
+		if (type == 0) {
+			String emsg = code.getAsString();
+			String msg = "";
+			if (emsg.equals("inactive")) msg = "You're not currently using Station";
+			if (emsg.equals("not in vc")) msg = "You're not in a voice channel.";
+			if (emsg.equals("wrong vc")) msg = "Station is already active in another voice channel.";
+			if (emsg.equals("queue maxed")) msg = "Your queue is full.";
+			if (emsg.equals("below 0")) msg = "Number can't be below 0.";
+			if (emsg.equals("nothing changed")) msg = "Nothing changed.";
+			if (emsg.equals("no songs")) msg = "You've reached the end of your queue.";
+			if (emsg.equals("invalid volume")) msg = "Invalid volume specified.";
+			if (emsg.equals("already paused")) msg = "Station is already paused.";
+			if (emsg.equals("already resumed")) msg = "Station is already resumed.";
+			if (emsg.equals("already looped")) msg = "Station is already looped.";
+			if (emsg.equals("already unlooped")) msg = "Station is already unlooped.";
+			if (emsg.equals("invalid values")) msg = "Invalid value specified.";
+			if (emsg.equals("invalid url")) msg = "Invalid URL specified.";
+			if (emsg.equals("rate limited")) {
+				if (command.equals("queue")) {
+					msg = "dontmessage";
+					send("queue", FabriStation.queueType, FabriStation.queueData, source);
+				} else {
+					msg = "This action was rate limited. Try again in a few seconds.";
+				}
+			}
+			if (emsg.equals("no text channel")) msg = "Your discord server isn't set up for Station.";
+			if (emsg.equals("invalid user token")) {
+				msg = "You're not connected to a Discord account.";
+				FabriStation.token = "";
+			}
+			if (emsg.equals("no track")) msg = "Couldn't find your music.";
+			if (emsg.equals("no station")) msg = "That station doesn't exist.";
+			if (emsg.equals("no queue")) {
+				if (command.equals("shuffle")) {
+					msg = "Your queue isn't long enough to shuffle.";
+				} else {
+					msg = "Your queue is empty.";
+				}
+			}
+			if (emsg.equals("inactive") || emsg.equals("not in vc") || emsg.equals("wrong vc") || emsg.equals("no songs") || emsg.equals("invalid user token") || (emsg.equals("no queue") && !command.equals("shuffle"))) {
+				FabriStation.artist = "";
+				FabriStation.title = "";
+			}
+			if (msg.equals("")) msg = "Something went wrong: " + emsg;
+
+			if (!msg.equals("dontmessage")) source.getPlayer().sendMessage(Text.of(msg), MessageType.SYSTEM, MinecraftClient.getInstance().player.getUuid());
+		} else {
+			String msg = "";
+
+			if (command.equals("tune")) {
+				JsonObject jsonObject = code.getAsJsonObject();
+				msg = "Tuning to §b" + jsonObject.get("displayname").getAsString() + "§f!";
+			} else if (command.equals("leave")) {
+				msg = "Station disconnected from your voice channel.";
+			} else if (command.equals("pause")) {
+				msg = "Paused.";
+			} else if (command.equals("resume")) {
+				msg = "Resumed.";
+			} else if (command.equals("shuffle")) {
+				msg = "Shuffled your queue.";
+			} else if (command.equals("clear")) {
+				msg = "Cleared your queue.";
+			} else if (command.equals("loop")) {
+				msg = "Looped current track.";
+			} else if (command.equals("unloop")) {
+				msg = "Unlooped current track";
+			} else if (command.equals("volume")) {
+				msg = "Set volume to §b%" + code.getAsString() + "§f.";
+			} else if (command.equals("next")) {
+				JsonObject jsonObject = code.getAsJsonObject();
+				msg = "Skipping to §b" + jsonObject.get("title").getAsString() + "§f by §b" + jsonObject.get("artist").getAsString() + "§f.";
+				FabriStation.artist = jsonObject.get("artist").getAsString();
+				FabriStation.title = jsonObject.get("title").getAsString();
+			} else if (command.equals("play")) {
+				JsonObject jsonObject = code.getAsJsonObject();
+				if (jsonObject.get("position").getAsInt() == 0) {
+					msg = "Playing §b" + jsonObject.get("title").getAsString() + "§f by §b" + jsonObject.get("artist").getAsString() + "§f.";
+					FabriStation.artist = jsonObject.get("artist").getAsString();
+					FabriStation.title = jsonObject.get("title").getAsString();
+				} else {
+					msg = "Queued §b" + jsonObject.get("title").getAsString() + "§f by §b" + jsonObject.get("artist").getAsString() + "§f.";
+				}
+			} else if (command.equals("stations")) {
+				msg = "\n§f§oTry this station";
+				JsonArray jsonArray = code.getAsJsonArray();
+				int i = 0;
+				while (i < jsonArray.size() && i < 1) {
+					JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+					msg += "\n\n§b§l" + jsonObject.get("displayname").getAsString() + "\n\n§7§o" + jsonObject.get("description").getAsString() + "\n§f   §n/s tune " + jsonObject.get("triggername").getAsString() + "\n";
+					i++;
+				}
+			} else if (command.equals("queue")) {
+				JsonArray jsonArray = code.getAsJsonObject().get("queue").getAsJsonArray();
+				int i = 1;
+				msg = "\n§f§oUpcoming tracks";
+				while (i < jsonArray.size() && i < 4) {
+					JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+					msg += "\n\n§f" + i + ". §7" + jsonObject.get("artist").getAsString() + "\n§b   " + jsonObject.get("title").getAsString();
+					i++;
+				}
+				if (i == 1) msg = "Your queue is empty.";
+				if (jsonArray.size() > 0) {
+					FabriStation.artist = jsonArray.get(0).getAsJsonObject().get("artist").getAsString();
+					FabriStation.title = jsonArray.get(0).getAsJsonObject().get("title").getAsString();
+				} else {
+					FabriStation.artist = "";
+					FabriStation.title = "";
+				}
+			}
+			if (msg.equals("")) {
+				source.getPlayer().sendMessage(Text.of("Success but unknown command: " + command), MessageType.SYSTEM, MinecraftClient.getInstance().player.getUuid());
+			} else {
+				source.getPlayer().sendMessage(Text.of(msg), MessageType.SYSTEM, MinecraftClient.getInstance().player.getUuid());
+			}
+		}
+	}
+
+
+}
---
+++ src/main/java/com/eyezah/station/Response.java
@@ -0,0 +1,47 @@
+package com.eyezah.station;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.command.ServerCommandSource;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Response {
+
+	public static void request(String request, String command, ServerCommandSource source) throws ParseException, IOException {
+		if (FabriStation.token.equals("")) {
+			FabriStation.error(source, "You're not connected to a Discord account.");
+		} else {
+			LOOKUP_THREAD.execute(() -> {
+				try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+					FabriStation.newRequest();
+					final HttpGet httpGet = new HttpGet(request);
+
+					try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+
+						String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+						JsonParser parser = new JsonParser();
+						JsonObject jsonObject = parser.parse(responseBody).getAsJsonObject();
+						MusicResponses.send(command, jsonObject.get("type").getAsInt(), jsonObject.get("code"), source);
+						FabriStation.doneRequest();
+					}
+				} catch (IOException | CommandSyntaxException e) {
+					e.printStackTrace();
+					FabriStation.doneRequest();
+					FabriStation.error(source);
+				}
+			});
+		}
+	}
+
+	private static final ExecutorService LOOKUP_THREAD = Executors.newSingleThreadExecutor();
+}
